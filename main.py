21e3285baff0b60347106a744bd9c336c2ca5fcc
Misha Lvovsky <m.lvovsky66@gmail.com>
2023-10-28 02:45:23 -0400

    changed gate positions

--- main.py
+++ main.py
@@ -138,16 +138,16 @@ vibe_config = TrainConfig.init(
     dispersion_weight=10.0,
     inverse_reconstruction_gate_sharpness=1,
     inverse_forward_gate_sharpness=1,
-    inverse_reconstruction_gate_center=-4,
-    inverse_forward_gate_center=-5,
+    inverse_reconstruction_gate_center=-6,
+    inverse_forward_gate_center=-6,
     forward_gate_sharpness=1,
     smoothness_gate_sharpness=1,
     dispersion_gate_sharpness=1,
     condensation_gate_sharpness=1,
     forward_gate_center=-2,
-    smoothness_gate_center=-2,
-    dispersion_gate_center=-2,
-    condensation_gate_center=-2,
+    smoothness_gate_center=-3,
+    dispersion_gate_center=-9,
+    condensation_gate_center=-9,
 )
 
 rng, key = jax.random.split(key)
